f94935d5047fcb3d6cc3ee97ceddee5c27c27aae
CalebKnight <caleb26502@gmail.com>
2022-06-13 13:42:20 +0800

    deleted unnecessary file

--- scipt.sh
+++
@@ -1,9 +0,0 @@
-# Using grep, regular expression, and pipe, write an executable Shell Script with a
-# single-line command to execute each of the following task:
-
-# • Print all the names that start with either ’A’ or ’B’, AND contains 5 letters or less.
-# (Save your answer as ”q2.sh”)
-# • Print all the names that contains substring ”am” (case insensitive), and then sort
-# the list alphabetically (ascending). (Hint: Do some research on which command
-# can be used to sort list) (Save your answer as ”q3.sh”)
-
